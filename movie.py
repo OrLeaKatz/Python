2245aa3c484e5b1e46f3f7dab2bbffc4b8c0f927
OrLeaKatz <44434354+OrLeaKatz@users.noreply.github.com>
2018-12-31 19:04:36 +0200

    Delete movie.py

--- movie.py
+++
@@ -1,7 +0,0 @@
-class Movie:
-    def __init__(self, name, length):
-        self.name = name
-        self.length = length
-
-    def get_info(self):
-        return "name: "+self.name +" ,length: " + str(self.length)
